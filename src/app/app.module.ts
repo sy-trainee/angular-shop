32a6d00dfd4ff728941b1a15f4451d3de49bfcd1
Sergey Yarygin <yason2000@gmail.com>
2021-01-18 20:54:56 +0200

    first component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent
   ],
   imports: [
     BrowserModule
