c5560f729e45c72e289b86a24b0a15bb105aa4a1
Sergey Yarygin <yason2000@gmail.com>
2021-03-08 17:37:49 +0200

    task 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { Router, RouterOutlet, NavigationEnd, NavigationStart } from '@angular/router';
+import { settings } from 'cluster';
+import { AppSettings } from './core/services/app-settings.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private appSettings: AppSettings) { }
+
+  ngOnInit(): void {
+    this.appSettings.getAppSettings().subscribe((settings) => {
+      console.log("Initial mode: " + settings.mode);
+    })
+  }
 
   onActivate($event: any, routerOutlet: RouterOutlet): void {
     console.log('Activated Component', $event, routerOutlet);
@@ -17,4 +27,3 @@ export class AppComponent {
   }
 
 }
-// linter
---
+++ src/app/core/services/app-settings.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { LocalStorageService } from './local-storage.service';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, publish, refCount, retry } from 'rxjs/operators';
+
+export class AppSettingsModel {
+    name: string;
+    mode: number;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AppSettings {
+
+    constructor(
+        private localStorage: LocalStorageService,
+        private http: HttpClient
+    ) { }
+
+    getAppSettings(): Observable<AppSettingsModel> {
+        var appSettings = this.loadFromStorage();
+        if (!appSettings) {
+            this.loadFromAssets().subscribe(
+                (settings) => {
+                    appSettings = settings;
+                },
+                (error) => {
+                    appSettings = this.getDefault();
+                    this.saveToStorage(appSettings);
+                });
+        }
+        return of(appSettings);
+    }
+
+    private loadFromAssets(): Observable<AppSettingsModel> {
+        return this.http.get<AppSettingsModel[]>("/app-settings.json").pipe(
+            retry(3),
+            publish(),
+            refCount(),
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        console.error('An error occurred:', err.error.message);
+        return throwError('Something bad happened; please try again later.');
+    }
+
+    private loadFromStorage(): AppSettingsModel {
+        const appSettingsValue: string = this.localStorage.get('app-settings');
+        if (appSettingsValue) {
+            return JSON.parse(appSettingsValue);
+        }
+    }
+
+    private saveToStorage(appSettings: AppSettingsModel): void {
+        this.localStorage.set('app-settings', JSON.stringify(appSettings));
+    }
+
+    private getDefault(): AppSettingsModel {
+        const appSettings = new AppSettingsModel();
+        appSettings.mode = 1;
+        appSettings.name = "vision";
+        return appSettings;
+    }
+
+}
